2e3c1f8d93fb10d94ffa1ccc915eff0ed3dde3b1
Jacky Chan <libing.chen@gmail.com>
2013-11-20 21:57:06 +0800

    Plantuml gist servlet

---
+++ src/main/java/org/mvnsearch/plantuml/gist/PlantumlGistServlet.java
@@ -0,0 +1,67 @@
+package org.mvnsearch.plantuml.gist;
+
+import com.google.gson.Gson;
+import net.sourceforge.plantuml.SourceStringReader;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Plantuml gist servlet
+ *
+ * @author linux_china
+ */
+public class PlantumlGistServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        String gistId = requestURI.replace("/gist/", "");
+        if (gistId.contains("?")) {
+            gistId = gistId.substring(0, gistId.indexOf("?"));
+        }
+        if (gistId.contains("/")) {
+            gistId = gistId.substring(0, gistId.indexOf("/"));
+        }
+        response.setContentType("image/png");
+        try {
+            String source = getGistContent(gistId);
+            if (source == null) {
+                response.sendRedirect("/img/gist_not_found.png");
+                return;
+            }
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            SourceStringReader reader = new SourceStringReader(source);
+            String desc = reader.generateImage(bos);
+            if ("(Error)".equals(desc)) {
+                response.sendRedirect("render_error.png");
+            } else {
+                response.getOutputStream().write(bos.toByteArray());
+            }
+        } catch (Exception e) {
+            response.sendRedirect("render_error.png");
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public String getGistContent(String id) throws IOException {
+        Gson gson = new Gson();
+        String httpUrl = "https://api.github.com/gists/" + id;
+        String responseBody = HttpClientUtils.getResponseBody(httpUrl);
+        Map json = gson.fromJson(responseBody, Map.class);
+        Map<String, Map<String, Object>> files = (Map<String, Map<String, Object>>) json.get("files");
+        for (Map.Entry<String, Map<String, Object>> entry : files.entrySet()) {
+            String fileName = entry.getKey();
+            if (fileName.endsWith(".puml")) {
+                Map<String, Object> file = entry.getValue();
+                return (String) file.get("content");
+            }
+        }
+        return null;
+    }
+
+}
