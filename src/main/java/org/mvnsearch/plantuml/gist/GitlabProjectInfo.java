2540aba7a7dc8eba75eb212b8341abbd11387daf
linux_china <linux_china@hotmail.com>
2014-06-19 18:18:10 +0800

    gitlab project

---
+++ src/main/java/org/mvnsearch/plantuml/gist/GitlabProjectInfo.java
@@ -0,0 +1,55 @@
+package org.mvnsearch.plantuml.gist;
+
+/**
+ * gitlab project
+ *
+ * @author linux_china
+ */
+public class GitlabProjectInfo {
+    /**
+     * id
+     */
+    private Integer id;
+    /**
+     * name
+     */
+    private String name;
+    /**
+     * path without "/" prefix
+     */
+    private String path;
+
+    public GitlabProjectInfo() {
+
+    }
+
+    public GitlabProjectInfo(Integer id, String name, String path) {
+        this.id = id;
+        this.name = name;
+        this.path = path;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
