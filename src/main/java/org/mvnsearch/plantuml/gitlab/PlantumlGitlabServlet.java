ea0324aab3f931a8b466499e8a007522adbe1c0b
linux_china <linux_china@hotmail.com>
2014-06-20 08:28:02 +0800

    Plantuml gitlab servlet

---
+++ src/main/java/org/mvnsearch/plantuml/gitlab/PlantumlGitlabServlet.java
@@ -0,0 +1,120 @@
+package org.mvnsearch.plantuml.gitlab;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+import net.sourceforge.plantuml.SourceStringReader;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
+import org.gitlab.api.GitlabAPI;
+import org.gitlab.api.http.GitlabHTTPRequestor;
+import org.gitlab.api.models.GitlabProject;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Plantuml gitlab servlet
+ *
+ * @author linux_china
+ */
+public class PlantumlGitlabServlet extends HttpServlet {
+    /**
+     * image cache
+     */
+    private Cache imageCache = CacheManager.getInstance().getCache("plantUmlImages");
+    private byte[] fileNotFound = null;
+    private byte[] noPumlFound = null;
+    private byte[] renderError = null;
+    private GitlabAPI gitlabAPI;
+    private GitlabInfo gitlabInfo = new GitlabInfo();
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        try {
+            gitlabAPI = GitlabAPI.connect(config.getInitParameter("gitlabUrl"), config.getInitParameter("userToken"));
+            initGitlabInfo();
+            fileNotFound = IOUtils.toByteArray(this.getClass().getResourceAsStream("/img/gist_not_found.png"));
+            noPumlFound = IOUtils.toByteArray(this.getClass().getResourceAsStream("/img/no_puml_found.png"));
+            renderError = IOUtils.toByteArray(this.getClass().getResourceAsStream("/img/render_error.png"));
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+
+    public void initGitlabInfo() throws Exception {
+        gitlabInfo.setUsername(gitlabAPI.getCurrentSession().getUsername());
+        for (GitlabProject gitlabProject : gitlabAPI.getProjects()) {
+            gitlabInfo.addProject(new GitlabProjectInfo(gitlabProject.getId(), gitlabProject.getName(), gitlabProject.getPathWithNamespace()));
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("image/png");
+        ServletOutputStream output = response.getOutputStream();
+        byte[] imageContent = renderError;
+        String requestURI = request.getRequestURI();
+        String filePath = requestURI.replace("/gitlab/", "");
+        Element element = imageCache.get(filePath);
+        if (filePath.endsWith(".puml")) {
+            if (element != null && !element.isExpired()) {  //cache
+                imageContent = (byte[]) element.getObjectValue();
+            } else {
+                try {
+                    String source = getGitlabFileContent(filePath);
+                    if (source == null) {  // gist not found
+                        imageContent = fileNotFound;
+                    } else if (source.equalsIgnoreCase("no puml found")) {  // no puml file found
+                        imageContent = noPumlFound;
+                    } else {  //render puml content
+                        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                        SourceStringReader reader = new SourceStringReader(source);
+                        String desc = reader.generateImage(bos);
+                        if (desc == null || !"(Error)".equals(desc)) {
+                            imageContent = bos.toByteArray();
+                            element = new Element(filePath, imageContent);
+                            imageCache.put(element);
+                        }
+                    }
+                } catch (Exception ignore) {
+
+                }
+            }
+        } else {
+            imageContent = noPumlFound;
+        }
+        output.write(imageContent);
+        output.flush();
+    }
+
+    /**
+     * get gitlab file content
+     *
+     * @param filePath file path
+     * @return puml file content
+     */
+    public String getGitlabFileContent(String filePath) throws Exception {
+        String projectPath = filePath.substring(0, filePath.indexOf("/blob/"));
+        GitlabProjectInfo project = gitlabInfo.findProject(projectPath);
+        String path = filePath.substring(filePath.indexOf("/blob/") + 6);
+        String branch = path.substring(0, path.indexOf("/"));
+        path = path.substring(path.indexOf("/") + 1);
+        GitlabHTTPRequestor retrieve = gitlabAPI.retrieve();
+        Map<String, Object> content = retrieve.to("/projects/" + project.getId() + "/repository/files?file_path=" + path + "&ref=" + branch, HashMap.class);
+        if (content.containsKey("content")) {
+            byte[] contents = Base64.decodeBase64((String) content.get("content"));
+            return new String(contents);
+        }
+        return null;
+    }
+
+}
