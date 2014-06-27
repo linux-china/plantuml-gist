5cdf6d6b2d4ae0875e1cb7a28b96cedd7274a60a
linux_china <linux_china@hotmail.com>
2014-06-27 16:34:46 +0800

    add image cache

--- src/main/java/org/mvnsearch/plantuml/gist/PlantumlGistServlet.java
+++ src/main/java/org/mvnsearch/plantuml/gist/PlantumlGistServlet.java
@@ -5,6 +5,7 @@ import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
 import net.sourceforge.plantuml.SourceStringReader;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.IOUtils;
 
 import javax.servlet.ServletConfig;
@@ -27,6 +28,8 @@ public class PlantumlGistServlet extends HttpServlet {
      * image cache
      */
     private Cache imageCache = CacheManager.getInstance().getCache("plantUmlImages");
+    private Cache uniqueImagesCache = CacheManager.getInstance().getCache("uniqueImages");
+    private Cache fileCache = CacheManager.getInstance().getCache("plantUmlFiles");
     private byte[] gistNotFound = null;
     private byte[] noPumlFound = null;
     private byte[] renderError = null;
@@ -66,18 +69,14 @@ public class PlantumlGistServlet extends HttpServlet {
                 } else if (source.equalsIgnoreCase("no puml found")) {  // no puml file found
                     imageContent = noPumlFound;
                 } else {  //render puml content
-                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                    SourceStringReader reader = new SourceStringReader(source);
-                    String desc = reader.generateImage(bos);
-                    if (desc == null || !"(Error)".equals(desc)) {
-                        imageContent = bos.toByteArray();
-                        element = new Element(gistId, imageContent);
-                        imageCache.put(element);
-                    }
+                    imageContent = renderPuml(gistId, source);
                 }
             } catch (Exception ignore) {
 
             }
+        }
+        if (imageContent == null) {
+            imageContent = gistNotFound;
         }
         output.write(imageContent);
         output.flush();
@@ -87,20 +86,49 @@ public class PlantumlGistServlet extends HttpServlet {
     public String getGistContent(String id) throws IOException {
         Gson gson = new Gson();
         String httpUrl = "https://api.github.com/gists/" + id;
-        String responseBody = HttpClientUtils.getResponseBody(httpUrl);
-        Map json = gson.fromJson(responseBody, Map.class);
-        if (json.containsKey("message") && "Not Found".equalsIgnoreCase((String) json.get("message"))) {
-            return null;
-        }
-        Map<String, Map<String, Object>> files = (Map<String, Map<String, Object>>) json.get("files");
-        for (Map.Entry<String, Map<String, Object>> entry : files.entrySet()) {
-            String fileName = entry.getKey();
-            if (fileName.endsWith(".puml")) {
-                Map<String, Object> file = entry.getValue();
-                return (String) file.get("content");
+        try {
+            String responseBody = HttpClientUtils.getResponseBody(httpUrl);
+            Map json = gson.fromJson(responseBody, Map.class);
+            if (json.containsKey("message") && "Not Found".equalsIgnoreCase((String) json.get("message"))) {
+                return null;
+            }
+            Map<String, Map<String, Object>> files = (Map<String, Map<String, Object>>) json.get("files");
+            for (Map.Entry<String, Map<String, Object>> entry : files.entrySet()) {
+                String fileName = entry.getKey();
+                if (fileName.endsWith(".puml")) {
+                    Map<String, Object> file = entry.getValue();
+                    Object content = file.get("content");
+                    fileCache.put(new Element(id, (String) content));
+                    return (String) content;
+                }
+            }
+        } catch (Exception e) {
+            Element element = fileCache.get(id);
+            if (element != null && !element.isExpired()) {
+                return (String) element.getObjectValue();
             }
         }
         return "no puml found";
+    }
+
+    public byte[] renderPuml(String gistID, String source) throws Exception {
+        String md5Key = DigestUtils.md5Hex(source);
+        Element element = uniqueImagesCache.get(md5Key);
+        if (element != null && !element.isExpired()) {
+            Object content = element.getObjectValue();
+            imageCache.put(new Element(gistID, content));
+            return (byte[]) content;
+        }
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        SourceStringReader reader = new SourceStringReader(source);
+        String desc = reader.generateImage(bos);
+        if (desc == null || !"(Error)".equals(desc)) {
+            byte[] imageContent = bos.toByteArray();
+            imageCache.put(new Element(gistID, imageContent));
+            uniqueImagesCache.put(new Element(md5Key, imageContent));
+            return imageContent;
+        }
+        return null;
     }
 
 }
