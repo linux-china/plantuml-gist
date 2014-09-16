68dfe1648eb8c43bea83e0958d247d1568feadb7
linux_china <linux_china@hotmail.com>
2014-09-16 12:16:36 +0800

    remove mapping

--- src/main/java/org/mvnsearch/plantuml/github/PlantumlGithubServlet.java
+++ src/main/java/org/mvnsearch/plantuml/github/PlantumlGithubServlet.java
@@ -30,7 +30,8 @@ public class PlantumlGithubServlet extends PlantUmlBaseServlet {
         ServletOutputStream output = response.getOutputStream();
         byte[] imageContent = renderError;
         String requestURI = request.getRequestURI();
-        String filePath = requestURI.replace("/github/", "/");
+        //remove mapping
+        String filePath = requestURI.substring(requestURI.indexOf("/", 1));
         if (filePath.endsWith(".puml")) {
             Element element = imageCache.get(filePath);
             if (element != null && !element.isExpired()) {  //cache
