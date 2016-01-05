e7afc31748cd8f157e49879123a8bebedd6702e1
linux_china <linux_china@hotmail.com>
2016-01-05 11:50:29 +0800

    gravizo servlet

---
+++ src/main/java/org/mvnsearch/plantuml/gravizo/GravizoServlet.java
@@ -0,0 +1,45 @@
+package org.mvnsearch.plantuml.gravizo;
+
+import org.mvnsearch.plantuml.PlantUmlBaseServlet;
+import org.mvnsearch.plantuml.gist.HttpClientUtils;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLDecoder;
+
+/**
+ * gravizo servlet
+ * <p/>
+ * demo url: http://puml.alibaba.net/g
+ *
+ * @author linux_china
+ */
+public class GravizoServlet extends PlantUmlBaseServlet {
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("image/png");
+        ServletOutputStream output = response.getOutputStream();
+        String content = null;
+        String query = request.getQueryString();
+        if (query != null) {
+            content = URLDecoder.decode(query, "utf-8").trim();
+        }
+        byte[] imageContent = renderPuml(content);
+        if (imageContent != null) {
+            output.write(imageContent);
+        } else {
+            output.write(renderError);
+        }
+        output.flush();
+    }
+
+}
